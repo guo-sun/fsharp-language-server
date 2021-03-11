5584322e601c29fbc2b521afd1a735b5fbeb1772
Steven <steven.s32@gmail.com>
2021-03-11 15:57:35 -0500

    Fix indent, only re-parse xml if needsUpdate

--- src/FSharpLanguageServer/TipFormatter.fs
+++ src/FSharpLanguageServer/TipFormatter.fs
@@ -79,34 +79,35 @@ let private ensure(xmlFile: FileInfo) =
                 match cache.TryGetValue(xmlFile.FullName) with 
                 | false, _ -> true
                 | _, existing -> existing.loadTime < xmlFile.LastWriteTime
+
             if needsUpdate then
-                dprintfn "Reading %s" xmlFile.FullName
+                dprintfn "Reading (%.2fMB) %s" ((float)xmlFile.Length / 1048576.0) xmlFile.FullName
 
-            let parsed = Dictionary<string, CachedMember>()
-                // The extension of these files is .xml, but they seem to actually be HTML
-                // For example, they contain unclosed <p> tags
+                let parsed = Dictionary<string, CachedMember>()
+                    // The extension of these files is .xml, but they seem to actually be HTML
+                    // For example, they contain unclosed <p> tags
 
-            let html = new HtmlDocument()
+                let html = new HtmlDocument()
 
-            html.Load(xmlFile.FullName)
-            // Find all members
-            for m in html.DocumentNode.Descendants("member") do 
-                convertSpecialTagsToMarkdown(m)
-                let name = m.GetAttributeValue("name", "")
-                let summary = [for e in m.Descendants("summary") do yield e.InnerHtml]
-                let parameters = [for e in m.Descendants("param") do yield e.GetAttributeValue("name", ""), e.InnerHtml]
-                let returns = [for e in m.Descendants("returns") do yield e.InnerHtml]
-                let exceptions = [for e in m.Descendants("exception") do yield cref(e), e.InnerHtml]
-                parsed.TryAdd(name, {
-                    summary = List.tryHead(summary)
-                    parameters = parameters 
-                    returns = List.tryHead(returns) 
-                    exceptions = exceptions
-                }) |> ignore
-                cache.TryAdd(xmlFile.FullName, {
-                    members = parsed 
-                    loadTime = xmlFile.LastWriteTime
-                }) |> ignore
+                html.Load(xmlFile.FullName)
+                // Find all members
+                for m in html.DocumentNode.Descendants("member") do 
+                    convertSpecialTagsToMarkdown(m)
+                    let name = m.GetAttributeValue("name", "")
+                    let summary = [for e in m.Descendants("summary") do yield e.InnerHtml]
+                    let parameters = [for e in m.Descendants("param") do yield e.GetAttributeValue("name", ""), e.InnerHtml]
+                    let returns = [for e in m.Descendants("returns") do yield e.InnerHtml]
+                    let exceptions = [for e in m.Descendants("exception") do yield cref(e), e.InnerHtml]
+                    parsed.TryAdd(name, {
+                        summary = List.tryHead(summary)
+                        parameters = parameters 
+                        returns = List.tryHead(returns) 
+                        exceptions = exceptions
+                    }) |> ignore
+                    cache.TryAdd(xmlFile.FullName, {
+                        members = parsed 
+                        loadTime = xmlFile.LastWriteTime
+                    }) |> ignore
         with
         | _ -> dprintfn "Couldn't load xml file %s" xmlFile.FullName
 
